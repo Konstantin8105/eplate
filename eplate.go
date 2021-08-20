9710994c1d971599b2f8f17f650653a75b4ea287
Konstantin8105 <konstantin8105@gmail.com>
2021-08-20 10:59:16 +0300

    verification is ok

--- eplate.go
+++ eplate.go
@@ -40,10 +40,18 @@ import (
 func main() {
 	var m ortho.Model
 	m.Init(1800, 1800, "base")
-	m.Add(100, "horizontal", 600, true)
-	m.Add(100, "horizontal", 1200, true)
-	ps, rs := m.Generate(500)
-	mainPlate, left, right, top, bottom := ortho.Select(ps)
+	m.Add(100, "stiff", 600, true)
+	m.Add(100, "stiff", 1200, true)
+	ps, rs := m.Generate(50)
+	ts := ortho.Select(ps)
+
+	thks := []struct {
+		name  string
+		value uint64
+	}{
+		{"base", 12},
+		{"stiff", 10},
+	}
 
 	var out string
 
@@ -52,57 +60,62 @@ func main() {
 		out += fmt.Sprintf("%5d,%5d,%5d,%5d\n", i+1, ps[i][0], ps[i][1], ps[i][2])
 	}
 
-	out += fmt.Sprintf("*ELEMENT, type=S4R, elset=eall\n")
-	for i := range rs {
-		out += fmt.Sprintf("%5d", i+1)
-		for _, p := range rs[i].PointsId {
-			out += fmt.Sprintf(",%5d", p+1)
+	// S4 or S4R
+	for _, t := range thks {
+		out += fmt.Sprintf("*ELEMENT, type=S4, elset=%s\n", t.name)
+		for i := range rs {
+			if t.name != rs[i].Material {
+				continue
+			}
+			out += fmt.Sprintf("%5d", i+1)
+			for _, p := range rs[i].PointsId {
+				out += fmt.Sprintf(",%5d", p+1)
+			}
+			out += fmt.Sprintf("\n")
 		}
-		out += fmt.Sprintf("\n")
 	}
 
 	out += fmt.Sprintf("*BOUNDARY\n")
 	boundary := make([][6]bool, len(ps))
-	for _, sl := range [][]int{left, right, top, bottom} {
-		for _, ind := range sl {
-			boundary[ind][2] = true // fix: Z
-			boundary[ind][5] = true // fix: MZ
+	for i := range ts {
+		if ts[i] == ortho.Other || ts[i] == ortho.MainPlate {
+			// do nothing
+			continue
+		}
+		boundary[i][2] = true // fix: Z
+		boundary[i][5] = true // fix: MZ
+		switch ts[i] {
+		case ortho.Left:
+		case ortho.Right:
+		case ortho.Top:
+		case ortho.Bottom:
+		case ortho.LeftTop:
+		case ortho.LeftBottom:
+			boundary[i][0] = true // fix: X
+			boundary[i][1] = true // fix: Y
+		case ortho.RightTop:
+		case ortho.RightBottom:
+			boundary[i][1] = true // fix: Y
+		default:
+			panic(ts[i])
 		}
 	}
-	for _, ind := range mainPlate {
-		if ps[ind][0] == 0 && ps[ind][1] == 0 && ps[ind][2] == 0 {
-			boundary[ind][0] = true // fix: X
-			break
-		}
-	}
-	{
-		var r uint64 = 0
-		for _, ind := range mainPlate {
-			if v := ps[ind][0]; r < v {
-				r = v
-			}
-		}
-		for _, ind := range mainPlate {
-			if (ps[ind][0] == r || ps[ind][0] == 0 ) && ps[ind][1] == 0 {
-				boundary[ind][1] = true // fix: Y
-			}
-		}
-	}
-	for _, ind := range mainPlate {
+	for i := range boundary {
 		for p := 0; p < 6; p++ {
-			if boundary[ind][p] == true {
-				out += fmt.Sprintf("%5d,%5d,%5d\n", ind+1, p+1, p+1)
+			if boundary[i][p] == true {
+				out += fmt.Sprintf("%5d,%5d,%5d\n", i+1, p+1, p+1)
 			}
 		}
 	}
 
 	out += fmt.Sprintf("*MATERIAL, name=Steel\n")
 	out += fmt.Sprintf("*ELASTIC\n")
-	out += fmt.Sprintf("210000,0.3\n")
-	out += fmt.Sprintf("*SHELL SECTION,ELSET=eall, material=steel\n")
+	out += fmt.Sprintf("205000,0.3\n")
 
-	// TODO
-	out += fmt.Sprintf("5\n")
+	for _, t := range thks {
+		out += fmt.Sprintf("*SHELL SECTION,ELSET=%s, material=steel\n", t.name)
+		out += fmt.Sprintf("%5d\n", t.value)
+	}
 
 	out += fmt.Sprintf("*STEP\n")
 
@@ -111,24 +124,50 @@ func main() {
 	out += fmt.Sprintf("2\n")
 
 	out += fmt.Sprintf("*CLOAD\n")
+	leftamount := 0
+	for i := range ts {
+		if ts[i] == ortho.Left {
+			leftamount++
+		}
+	}
+	leftamount += 2
+	sigma := 1.0 // N/mm2
+	force := 1800.0 * 12.0 * sigma / float64(leftamount-1)
 
 	// TODO
-	for _, ind := range left {
-		out += fmt.Sprintf("%5d,1,720\n", ind+1,)
+	for i := range ts {
+		if ts[i] == ortho.Left {
+			out += fmt.Sprintf("%5d,1,%+.6e\n", i+1, force)
+		}
+		if ts[i] == ortho.LeftBottom {
+			out += fmt.Sprintf("%5d,1,%++.6e\n", i+1, force/2)
+		}
+		if ts[i] == ortho.LeftTop {
+			out += fmt.Sprintf("%5d,1,%+.6e\n", i+1, force/2)
+		}
+		if ts[i] == ortho.Right {
+			out += fmt.Sprintf("%5d,1,%+.6e\n", i+1, -force)
+		}
+		if ts[i] == ortho.RightTop {
+			out += fmt.Sprintf("%5d,1,%+.6e\n", i+1, -force/2)
+		}
+		if ts[i] == ortho.RightBottom {
+			out += fmt.Sprintf("%5d,1,%+.6e\n", i+1, -force/2)
+		}
 	}
 	// out += fmt.Sprintf("6,3,-10000\n")
 
-	out += fmt.Sprintf("*EL PRINT,elset=eall\n")
-
-	out += fmt.Sprintf("s\n")
+// 	out += fmt.Sprintf("*EL PRINT,elset=eall\n")
+// 
+// 	out += fmt.Sprintf("s\n")
 
 	out += fmt.Sprintf("*NODE FILE\n")
 
 	out += fmt.Sprintf("u\n")
 
-	out += fmt.Sprintf("*EL FILE, output=3D\n")
-
-	out += fmt.Sprintf("s\n")
+ 	out += fmt.Sprintf("*EL FILE, output=3D\n")
+ 
+ 	out += fmt.Sprintf("s\n")
 
 	out += fmt.Sprintf("*END STEP\n")
 
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Konstantin8105/eplate
 
 go 1.16
 
-require github.com/Konstantin8105/ortho v1.0.1-0.20210819074600-48a07cbcee89
+require github.com/Konstantin8105/ortho v1.0.1-0.20210819200609-8f1b7c8f2cfe
